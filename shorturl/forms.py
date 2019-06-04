7cd7a902bd4f81d93769306a3028d9033af7e599
Adrish Maity <adrish.maity@outlook.com>
2019-06-04 21:15:54 +0530

    Completed shorten link functionality

--- shorturl/forms.py
+++ shorturl/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
 class ShortenURLForm(forms.Form):
-    url = forms.URLField(label='Your website', required=True)
+    url = forms.URLField(label='Your website', required=True,
+                        widget=forms.TextInput(attrs={'placeholder': 'Your website'}))
--- shorturl/views.py
+++ shorturl/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from .forms import ShortenURLForm
+import requests
+import json
+import urllib
+from bs4 import BeautifulSoup
+
+
 
 # Create your views here.
 def homePage(request):
@@ -7,13 +13,49 @@ def homePage(request):
     if request.method == 'POST':
         transaction = ShortenURLForm(request.POST)
         if transaction.is_valid():
-            print(transaction.cleaned_data)
+            cleaned_data = transaction.cleaned_data
+            print(cleaned_data)
+            enteredURL = cleaned_data['url']
 
+            try:
+                soup = BeautifulSoup(urllib.request.urlopen(cleaned_data['url']))
+
+            except urllib.error.URLError as e:
+                print(e.reason)
+                error = e.reason
+                result_data = ''
+
+            else:
+                print(soup.title.string)
+
+                # curl -H "public-api-token: 52b1f183416b101f7096fbf897f2c674" -X PUT -d "urlToShorten=google.com" https://api.shorte.st/v1/data/url
+                api_url = 'https://api.shorte.st/v1/data/url'
+
+                data = {'urlToShorten': cleaned_data['url']}
+                params = {'public-api-token': '52b1f183416b101f7096fbf897f2c674'}
+
+                result = requests.put(api_url, headers=params, data=json.dumps(data))
+                json_data = json.loads(result.text)
+                result_data = json_data['shortenedUrl']
+                print(result_data)
+                error = False
+
+        else:
+            result_data = ''
+            error = False
+            enteredURL = ''
+
+    else:
+        error = False
+        result_data = ''
+        enteredURL = ''
 
     transaction = ShortenURLForm()
 
     context = {
-        'result': '',
+        'error': error,
+        'url_entered': enteredURL,
+        'result': result_data,
         'url_form': transaction
     }
 
