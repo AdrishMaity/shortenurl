c0f187eabe9ecf27499f9c8f4ce8ac1481ac16c2
Adrish Maity <adrish.maity@outlook.com>
2019-06-04 17:24:47 +0530

    Created ShortenURL model, superuser, base template and commited all changes in db

--- shorturl/models.py
+++ shorturl/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.validators import URLValidator
 
 # Create your models here.
+class ShortenURL(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Shorten URL"
+
+    def __str__(self):
+        return self.id + '_' + self.linkTitle
+
+    linkTitle = models.CharField(max_length=100, default="A Site")
+    urlToBeShortened = models.CharField(max_length=500, validators=[URLValidator()])
+    shortenedUrl = models.CharField(max_length=300, validators=[URLValidator()])
