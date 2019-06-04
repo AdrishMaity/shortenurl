9f4c0b59ccd7dfd9c3fbf55f969a33f3d4ca8fec
Adrish Maity <adrish.maity@outlook.com>
2019-06-04 17:42:17 +0530

    created form to search and submit url for shorten url

--- shorturl/views.py
+++ shorturl/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .forms import ShortenURLForm
 
 # Create your views here.
 def homePage(request):
-    return render(request, "shorturl/home.html")
+
+    if request.method == 'POST':
+        transaction = ShortenURLForm(request.POST)
+        if transaction.is_valid():
+            print(transaction.cleaned_data)
+
+
+    transaction = ShortenURLForm()
+
+    context = {
+        'result': '',
+        'url_form': transaction
+    }
+
+    return render(request, "shorturl/home.html", context)
